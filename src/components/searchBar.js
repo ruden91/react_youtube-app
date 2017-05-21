b8167f1bbb7a8483f33d82dfdfcacde1f33b6ff5
Ruden <ruden91@naver.com>
2017-05-21 18:36:16 +0900

    add constructor and state

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
     render() {
-        return <input />;
+        return <input onChange={this.onInputChange} />;
+    }
+
+    onInputChange(e) {
+        console.log(e.target.value)
     }
 }
 
