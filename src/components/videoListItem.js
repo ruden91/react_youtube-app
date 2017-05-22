04dd6304f96034c94d90d6bc5daf6e2ad4ee73b6
Ruden <ruden91@naver.com>
2017-05-23 00:43:29 +0900

    revise videoListItem module

--- src/components/videoListItem.js
+++ src/components/videoListItem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
-const VideoListItem = (props) => {
-    console.log(props);
+const VideoListItem = ({video}) => {
+    // const video = props.video;
+    const imageUrl = video.snippet.thumbnails.default.url;
+
     return (
-    <li>
-        <img src={props.video.snippet.thumbnails.default.url} />
-        <p>{props.video.snippet.title}</p>
+    <li className="list-group-item">
+        <div className="video-list media">
+            <div className="video-left">
+                <img className="media-object" src={imageUrl} />
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </div>
     </li>
 )
 }
